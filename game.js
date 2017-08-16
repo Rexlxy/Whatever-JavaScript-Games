344781db0a67e64210098920519dab5af777cc8d
Rex Liu <1995rexlxy@gmail.com>
2017-08-17 00:05:31 +0800

    Eventlistener and game.runGame()
    put all listeners into game object and enable Game object to run the game

--- game.js
+++ game.js
@@ -1,43 +1,53 @@
 var Game = function(){
   var g = {
-    gameName:110
+    keyDowns:{},
+    keyFunctions:{}
   }
   g.canvas = document.getElementById("myCanvas")
   g.context = g.canvas.getContext('2d')
 
   g.clear = function(){
-  g.context.clearRect(0,0, 400,400)
+  g.context.clearRect(0, 0, 400, 400)
 }
 
   //判断是否游戏结束
   g.checkGG = function(count){
-
     //球掉下去，游戏结束
-    if(ball.y > 350){
-    document.getElementById("status").innerHTML = "呵呵，垃圾"
-    //蒙一层色
-    g.drawColorLayer()
-    //Game over
-    g.drawText('Game Over', 10, 205, '#FF6A6A','bold 70px arial' )
-
-    //分数
-    g.drawText("Score:", 160, 280, 'FF6A6A','bold 20px arial' )
-    g.drawText(count, 230, 280, 'FF6A6A','bold 20px arial' )
-    return true
+    if (ball.y > 350){
+      document.getElementById("status").innerHTML = "呵呵，垃圾"
+      //蒙一层色
+      g.drawColorLayer()
+      //Game over
+      g.drawText('Game Over', 10, 205, '#FF6A6A', 'bold 70px arial' )
+      //分数
+      g.drawText("Score:", 160, 280, 'FF6A6A', 'bold 20px arial' )
+      g.drawText(count, 230, 280, 'FF6A6A', 'bold 20px arial' )
+      return true
     }
 
     //砖块打光，过关
-    if(count == bricks.length){
-    document.getElementById("status").innerHTML = "You win"
+    if (count == bricks.length){
+      document.getElementById("status").innerHTML = "You win"
       // window.alert("GG")
-    g.drawColorLayer()
-    g.drawText('You win', 50, 205, 'FF6A6A','bold 70px arial' )
-    return true
+      g.drawColorLayer()
+      g.drawText('You win', 50, 205, 'FF6A6A', 'bold 70px arial' )
+      return true
     }
     return false
   }
 
-  //draw text 
+  window.addEventListener('keydown', function(event){
+    g.keyDowns[event.keyCode] = true
+  })
+
+  window.addEventListener('keyup', function(event){
+    g.keyDowns[event.keyCode] = false
+  })
+
+  g.registerKey = function(keyCode, callback){
+    g.keyFunctions[keyCode] = callback
+  }
+  //draw text
   g.drawText = function(text, x, y, color, font){
     g.context.font = font
     g.context.fillStyle = color
@@ -45,8 +55,8 @@ var Game = function(){
   }
 
   g.drawColorLayer = function(){
-        g.context.fillStyle="rgba(255,165,0,0.8)"
-    g.context.fillRect(0,0,400,400)
+    g.context.fillStyle = "rgba(255,165,0,0.8)"
+    g.context.fillRect(0, 0, 400, 400)
   }
 
   g.draw = function(img){
@@ -54,19 +64,31 @@ var Game = function(){
 }
 
   g.drawScore = function(score){
-    g.drawText("Score:", 10, 380, 'blue','bold 16px arial' )
-    g.drawText(score, 60, 380, 'blue','bold 16px arial' )
+    g.drawText("Score:", 10, 380, 'blue', 'bold 16px arial' )
+    g.drawText(score, 60, 380, 'blue', 'bold 16px arial' )
   }
 
   g.collide = function(object1, object2){
-      if(checkAcrossB(object1, object2)){
+      if (checkAcrossB(object1, object2)){
         return true
       }
-      if(checkAcrossB(object2, object1)){
+      if (checkAcrossB(object2, object1)){
         return true
       }
       return false
     }
 
+  g.runGame = function(){
+    var keys = Object.keys(g.keyFunctions)
+    for(var i = 0; i < keys.length; i++){
+      var keyCode = keys[i]
+      // 如果对应键被按下
+      if (g.keyDowns[keyCode]){
+        g.keyFunctions[keyCode]()
+      }
+    }
+    //更新球的位置
+    g.update()
+  }
 return g
 }
